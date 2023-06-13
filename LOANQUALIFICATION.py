0c40f334d7141f1622465aeeac5a12f85e050b36
mwanziadanny <135580549+mwanziadanny@users.noreply.github.com>
2023-06-13 10:48:05 +0300

    Add files via upload

---
+++ LOANQUALIFICATION.py
@@ -0,0 +1,8 @@
+age = int(input('enter age: '))
+annual_income= int(input('enter annual income: '))
+
+if age>=21 and annual_income>=21000:
+
+    print("congratulations you qualify for a loan")
+else:
+    print("unfortunately, we are unable to offer you a loan at this time")
